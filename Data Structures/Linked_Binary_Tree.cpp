60eb9a435b13ed1b3bb6380a744fb76f43d0ad34
Narayan <narayandabu123@gmail.com>
2022-12-19 17:24:46 +0530

    added BST in BT

--- Data Structures/Linked_Binary_Tree.cpp
+++ Data Structures/Linked_Binary_Tree.cpp
@@ -5,23 +5,22 @@ using namespace std;
 
 template <class Any>
 class Binary_Tree{
-
     protected:
         Any data;
         Binary_Tree * left=NULL;
         Binary_Tree * right=NULL;
     public:
-        Binary_Tree(Any data){
+        Binary_Tree(Any data){ // Default Constructor 
             Any * node = new Any;
             this->data=data;
         }
-        void link_left(Binary_Tree*left){
+        void link_left(Binary_Tree*left){ // To Link Left subBT
             this->left=left;
         }
-        void link_right(Binary_Tree*right){
+        void link_right(Binary_Tree*right){ // To Link Right subBT
             this->right=right;
         }
-        void preorder(Binary_Tree*root){
+        void preorder(Binary_Tree*root){ // To Perform preorder traversal
            if(root!= NULL){
             cout<<root->data<<" ";
             preorder(root->left);
@@ -29,7 +28,7 @@ class Binary_Tree{
         }
            return;
         }
-        void postorder(Binary_Tree*root){
+        void postorder(Binary_Tree*root){ // To Perform postorder traversal
             if(root!= NULL){
                 postorder(root->left);
                 postorder(root->right);
@@ -37,7 +36,7 @@ class Binary_Tree{
             }
             return;
         }
-        void inorder(Binary_Tree*root){
+        void inorder(Binary_Tree*root){ // To Perform inorder traversal
             if(root!= NULL){
                 inorder(root->left);
                 cout<<root->data<<" ";
@@ -46,26 +45,127 @@ class Binary_Tree{
             return;
 
         }
-
+        bool isBST(Binary_Tree*root){ // To Check if a BT is a BST
+        if (root == NULL)return true;
+        if (root->left != NULL && root->left->data > root->data)
+            return false;
+        if (root->right != NULL && root->right->data < root->data)
+            return false;
+        if (!isBST(root->left) || !isBST(root->right))
+            return false;
+        return true;
+        }
+        Any getdata(Binary_Tree* root){ // Returns the data in the node
+            return root->data;   
+        }
+        Binary_Tree * bstsearchrecu(Binary_Tree *root, Any key){ // BS in BST recursively
+            if (root==NULL){
+                return NULL;
+            }
+            if (key==root->data)
+            {
+                return root;
+            }
+            else if (key<root->data)
+            {
+                return bstsearchrecu(root->left,key);
+            }
+            else
+            {
+                return bstsearchrecu(root->right,key);
+            }  
+        }
+        Binary_Tree * bstsearchiter(Binary_Tree *root, Any key){ // BS in BST iteratively 
+            while (root!=NULL){
+                if(root->data==key){
+                    return root;
+                }
+                else if(root->data>key){
+                    root=root->left;
+                }
+                else{
+                    root=root->right;
+                }
+            }
+            return NULL;
+        }
+        void sresult(Binary_Tree * root,Any key, char type){ // Returns if the element is found or not
+           if(type=='r'){ 
+                if((root->bstsearchrecu(root,key))==NULL)
+                {
+                    cout<<"Element not found"<<endl;
+                }
+                else
+                {
+                     cout<<"Element found"<<endl;
+                }
+           }
+           else if (type=='i')
+           {
+                if((root->bstsearchiter(root,key))==NULL)
+                {
+                    cout<<"Element not found"<<endl;
+                }
+                else
+                {
+                     cout<<"Element found"<<endl;
+                }
+           }
+           else
+           {
+            cout<<"Select a Particular Mode 'i' or 'r' ."<<endl;
+           }
+        }
+        
 };
 
 
 
 int main (){       
-    Binary_Tree<int> root(4);
-    Binary_Tree<int> rootl(42);
-    Binary_Tree<int> rootr(67);
-    Binary_Tree<int> rootl1(89);
-    Binary_Tree<int> rootr1(56);
+    // Making Binary tree...
+    
+    //Normal Random BT
+    // Binary_Tree<int> root(4);                       
+    // Binary_Tree<int> rootl(42);                     
+    // Binary_Tree<int> rootr(67);
+    // Binary_Tree<int> rootl1(89);
+    // Binary_Tree<int> rootr1(56);
+
+    // For a  BST
+    Binary_Tree<int> root(10);                       
+    Binary_Tree<int> rootl(7);                     
+    Binary_Tree<int> rootr(20);
+    Binary_Tree<int> rootl1(3);
+    Binary_Tree<int> rootr1(8);
+    /*  Representation of the Binary Tree...
+                   (4)
+                   / \
+                 (42)(67)   
+                 / \ 
+               (89)(56)
+    */
+
+
+    // Linking...
     root.link_left(&rootl);
     root.link_right(&rootr);
     rootl.link_left(&rootl1);
     rootl.link_right(&rootr1);
-    root.preorder(&root);
-    cout<<"preorder"<<endl;
-    root.postorder(&root);
-    cout<<"postorder"<<endl;
-    root.inorder(&root);
-    cout<<"inorder"<<endl;
+
+    // Functions Call...
+
+
+    // root.preorder(&root);
+    // cout<<"preorder"<<endl;
+    // root.postorder(&root);
+    // cout<<"postorder"<<endl;
+    // root.inorder(&root);
+    // cout<<"inorder"<<endl;
+    // cout<<root.isBST(&root)<<endl;
+           
+    root.sresult(&root,7,'i');
+    // cout<<root.getdata(root.bstsearchrecu(&root,89))<<endl;
+    // cout<<root.getdata(root.bstsearchiter(&root,10))<<endl;
+
     return 0;
 }
